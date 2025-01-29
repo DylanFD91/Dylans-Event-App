bbc9149c41fe58497f0fc5158c726259e567d899
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2025-01-29 15:48:37 -0600

    Adding React Router and cleaning up App to prep for adding back functionality.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Route.tsx'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
