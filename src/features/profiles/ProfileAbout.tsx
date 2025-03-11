22844819a86fc601d8f6e31ed27378580001c17a
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2025-03-11 17:40:02 -0500

    Added basic photo upload skeleton, will add functionality next.

--- src/features/profiles/ProfileAbout.tsx
+++ src/features/profiles/ProfileAbout.tsx
@@ -2,6 +2,7 @@ import { Button, Grid, Header, Tab } from "semantic-ui-react";
 import { Profile } from "../../app/types/profile";
 import { useState } from "react";
 import ProfileForm from "./ProfileForm";
+import { auth } from "../../app/config/firebase";
 
 type Props = {
     profile: Profile
@@ -9,17 +10,21 @@ type Props = {
 
 export default function ProfileAbout({profile}: Props) {
     const [editMode, setEditMode] = useState(false);
+    const isCurrentUser = auth.currentUser?.uid === profile.id;
+
+    
     return (
         <Tab.Pane>
             <Grid>
                 <Grid.Column width={16}>
                     <Header floated='left' icon='user' content={`About ${profile.displayName}`} />
+                    {isCurrentUser &&
                     <Button 
                         floated='right'
                         basic
                         content={editMode ? 'Cancel' : 'Edit Profile'}
                         onClick={() => setEditMode(!editMode)}
-                    />
+                    />}
                 </Grid.Column>
                 <Grid.Column width={16}>
                     {editMode ? <ProfileForm profile={profile} setEditMode={setEditMode}/> : (
