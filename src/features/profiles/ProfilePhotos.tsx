118bf93ea96db79db7f4a59bb1e2baa6ab3c942f
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2025-03-18 16:43:23 -0500

    added listening to the photo uploads

--- src/features/profiles/ProfilePhotos.tsx
+++ src/features/profiles/ProfilePhotos.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Tab, Grid, Header, Button, Card, Image } from "semantic-ui-react";
 import { Profile } from "../../app/types/profile";
 import { auth } from "../../app/config/firebase";
 import PhotoUpload from "./PhotoUpload";
+import { useAppSelector } from "../../app/store/store";
+import { useFireStore } from "../../app/hooks/firestore/useFirestore";
+import { actions } from "./photosSlice";
 
 type Props = {
     profile: Profile
@@ -11,9 +14,15 @@ type Props = {
 export default function ProfilePhotos({profile}: Props) {
     const [editMode, setEditMode] = useState(false);
     const isCurrentUser = auth.currentUser?.uid === profile.id;
+    const {data: photos, status} = useAppSelector(state => state.photos);
+    const {loadCollection} = useFireStore(`profiles/${profile.id}/photos`);
+
+    useEffect(() => {
+        loadCollection(actions)
+    }, [loadCollection])
 
     return (
-        <Tab.Pane>
+        <Tab.Pane loading={status === 'loading'}>
             <Grid>
                 <Grid.Column width={16}>
                     <Header floated='left' icon='photo' content='Photos' />
@@ -28,14 +37,16 @@ export default function ProfilePhotos({profile}: Props) {
                 <Grid.Column width={16}>
                     {editMode ? <PhotoUpload profile={profile} setEditMode={setEditMode}/> : (
                         <Card.Group itemsPerRow={5}>
-                            <Card>
-                                <Image src='/user.png'/>
-                                {isCurrentUser &&
-                                <Button.Group>
-                                    <Button basic color='green'>Main</Button>
-                                    <Button basic color='red' icon='trash' />
-                                </Button.Group>}
-                            </Card>
+                            {photos.map(photo => (
+                                <Card key={photo.id}>
+                                    <Image src={photo.url}/>
+                                    {isCurrentUser &&
+                                    <Button.Group>
+                                        <Button basic color='green'>Main</Button>
+                                        <Button basic color='red' icon='trash' />
+                                    </Button.Group>}
+                                </Card>
+                            ))}
                         </Card.Group>
                     )}
                 </Grid.Column>
